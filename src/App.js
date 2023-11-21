f34da1d671abe360adbdf92e1d40e03d31e90e63
Aurelie715 <aureliebernard29@gmail.com>
2023-11-21 17:13:13 +0100

    installation of react-router-dom and sass + path creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import Root from './Root.jsx';
+import Home from './view/Home.jsx';
+import ErrorPage from './view/ErrorPage.jsx';
+import About from './view/About.jsx'; 
+import Contact from './view/Contact.jsx';
+import Portfolio from './view/Portfolio.jsx';
+import Menu from './view/Menu.jsx';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
+      <Route index element={<Home />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/menu" element={<Menu />} />
+    </Route>
+  )
+)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
